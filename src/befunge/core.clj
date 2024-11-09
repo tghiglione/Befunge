2f4a2dd50dc59297dd763e34bc48949776f12b26
tomasghiglione <tghiglione@fi.uba.ar>
2024-11-09 11:19:36 -0300

    creacion repositorio clojure

---
+++ src/befunge/core.clj
@@ -0,0 +1,6 @@
+(ns befunge.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/befunge/core_test.clj
@@ -0,0 +1,7 @@
+(ns befunge.core-test
+  (:require [clojure.test :refer :all]
+            [befunge.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
