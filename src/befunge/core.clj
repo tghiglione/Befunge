27d48f0c3d5515f6962b6fad7ad1dd4dffade302
eliascuba <ecuba@fi.uba.ar>
2024-11-18 16:37:12 -0300

    Entrega TP 2 Befunge

--- src/befunge/core.clj
+++ src/befunge/core.clj
@@ -137,10 +137,124 @@
         (assoc :stack rest)
         (assoc-in [:pc :dir] dir))))
 
-;; Función -main
+;; Función que desapila y, x, obtiene el valor en esa posición de la grilla y lo apila
+(defn command-get
+  [estado]
+  (let [stack (:stack estado)
+        y (or (first stack) 0)
+        x (nth stack 1 0)
+        rest (drop 2 stack)
+        grid (:grid estado)
+        within-range? (and (<= 0 x (dec grid-columnas))
+                           (<= 0 y (dec grid-filas)))
+        value (if within-range?
+                (int (get-in grid [y x] \0))
+                0)]
+    (assoc estado :stack (conj rest value))))
+
+;; Funcion que desapila x,y, valor y coloca el caracter en la grilla
+(defn command-put
+  [estado]
+  (let [stack (:stack estado)
+        y (or (first stack) 0)
+        x (nth stack 1 0)
+        v (nth stack 2 0)
+        rest (drop 3 stack)
+        grid (:grid estado)
+        within-range? (and (<= 0 x (dec grid-columnas))
+                           (<= 0 y (dec grid-filas)))]
+    (if within-range?
+      (let [char (char v)
+            new-grid (assoc-in grid [y x] char)]
+        (-> estado
+            (assoc :stack rest)
+            (assoc :grid new-grid)))
+      (assoc estado :stack rest))))
+
+;; Función principal para interpretar comandos
+(defn interpretador-comando
+  [estado comando]
+  (let [{:keys [stack pc grid]} estado]
+    (cond
+      ;; Números del 0 al 9
+      (Character/isDigit comando)
+      (update estado :stack conj (Character/digit comando 10))
+
+      (= comando \>) (assoc-in estado [:pc :dir] :right)
+      (= comando \<) (assoc-in estado [:pc :dir] :left)
+      (= comando \^) (assoc-in estado [:pc :dir] :up)
+      (= comando \v) (assoc-in estado [:pc :dir] :down)
+
+      (= comando \#) (update estado :pc mover-pc)
+
+      (= comando \") (assoc estado :string-mode true)
+
+      (= comando \+) (operacion-binaria estado +)
+      (= comando \-) (operacion-binaria estado -)
+      (= comando \*) (operacion-binaria estado *)
+      (= comando \/) (operacion-binaria estado (fn [b a] (if (zero? a) 0 (quot b a))))
+      (= comando \%) (operacion-binaria estado (fn [b a] (if (zero? a) 0 (mod b a))))
+
+      (= comando \!) (operacion-unaria estado (fn [a] (if (zero? a) 1 0)))
+      (= comando \`) (operacion-binaria estado (fn [b a] (if (> b a) 1 0)))
+
+      (= comando \.) (imprimir-entero estado)
+      (= comando \,) (imprimir-char estado)
+      (= comando \&) (input-int estado)
+      (= comando \~) (input-char estado)
+
+      (= comando \_) (horizontal-if estado)
+      (= comando \|) (vertical-if estado)
+      (= comando \?) (assoc-in estado [:pc :dir] (rand-nth [:up :down :left :right]))
+
+      (= comando \:) (duplicar-top estado)
+      (= comando \\) (intercambiar-top estado)
+      (= comando \$) (assoc estado :stack (rest (:stack estado)))
+
+      (= comando \g) (command-get estado)
+      (= comando \p) (command-put estado)
+
+      (= comando \@) (assoc estado :finished true)
+
+      (= comando \space) estado
+
+      :else
+      (do
+        (println (str "Comando no reconocido o no implementado: " comando))
+        estado))))
+
+;; Función principal de ejecución
+(defn ejecutar-programa
+  [estado]
+  (if (:finished estado)
+    (println "\nEjecución finalizada.")
+    (let [{:keys [pc grid stack string-mode]} estado
+          {:keys [x y dir]} pc
+          current-cell (get-in grid [y x])]
+      (if string-mode
+        ;; Modo cadena
+        (if (= current-cell \")
+          (let [new-state (assoc estado :string-mode false)
+                next-pc (mover-pc (:pc new-state))
+                updated-state (assoc new-state :pc next-pc)]
+            (recur updated-state))
+          (let [new-state (update estado :stack conj (int current-cell))
+                next-pc (mover-pc (:pc new-state))
+                updated-state (assoc new-state :pc next-pc)]
+            (recur updated-state)))
+        ;; Modo normal
+        (let [new-state (interpretador-comando estado current-cell)
+              next-pc (mover-pc (:pc new-state))
+              updated-state (assoc new-state :pc next-pc)]
+          (recur updated-state))))))
+
 (defn -main
   [& args]
   (let [file-path (first args)]
     (if file-path
-      (println "Ruta del archivo Befunge-93:" file-path)
+      (let [program-lines (read-program file-path)
+            grid (crear-grilla program-lines)
+            estado-inicial (assoc estado-inicial :grid grid)]
+        ;; Ejecutar el programa
+        (ejecutar-programa estado-inicial))
       (println "Por favor, proporciona la ruta del archivo Befunge-93 a ejecutar."))))
